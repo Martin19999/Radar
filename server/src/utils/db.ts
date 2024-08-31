41cefcce0b9548d5ac3ba38756572c83b09a3ec1
Jianhao Feng <119100251@umail.ucc.ie>
2024-08-31 12:56:43 +0100

    Fix TypeScript version etc for deployment

--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -4,7 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  // connectionString: process.env.DATABASE_URL
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: parseInt(process.env.PGPORT ?? '5432', 10),
+  database: process.env.PGDATABASE
 });
 
 export const query = (text: string, params?: any[]) => pool.query(text, params);
