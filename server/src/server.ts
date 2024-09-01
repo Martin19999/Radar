7af6090b3728afdfa6239e32e4307497a4b4f669
Jianhao Feng <119100251@umail.ucc.ie>
2024-09-01 21:32:41 +0100

    dp

--- server/src/server.ts
+++ server/src/server.ts
@@ -27,6 +27,7 @@ app.options('*', cors(corsOptions));
 
 app.use(function(req, res, next) {
   if (req.method === 'OPTIONS') {
+    console.log(req)
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PUT');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
