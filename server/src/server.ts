5d29077b201cb94c9e4e2deae7110c4ff2ab9463
Jianhao Feng <119100251@umail.ucc.ie>
2024-09-02 09:02:29 +0100

    dp

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,6 +15,17 @@ const cors = require('cors');
 const app = express();
 
 var whitelist = [process.env.REACT_APP_FRONTEND_URL, 'http://localhost:3000']
+
+app.use(function(req, res, next) {
+  const origin = req.header('Origin');
+  console.log("hey");
+  res.setHeader("Access-Control-Allow-Origin", origin!);
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+  
+});
+
 var corsOptions = {
   origin: true,
   optionsSuccessStatus: 200,
@@ -23,22 +34,21 @@ var corsOptions = {
 }
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); 
-
-app.use(function(req, res, next) {
+app.options("*", (req, res) => {
   const origin = req.header('Origin');
   res.setHeader("Access-Control-Allow-Origin", origin!);
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-  
+  res.sendStatus(200);
 });
+
+
 
 app.use(express.json());
 
 
 // Serve static files from the public directory in the app folder
-app.use(express.static(path.join(__dirname, '../app/public')));
+// app.use(express.static(path.join(__dirname, '../app/public')));
 
 // Use routes
 app.use('/api/users', userUpdateRoutes);
