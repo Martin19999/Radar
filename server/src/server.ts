3ec49b01f51799da1f86124146ee6fcaebb23cea
Jianhao Feng <119100251@umail.ucc.ie>
2024-09-02 13:48:19 +0100

    dp

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,33 +14,33 @@ const cors = require('cors');
 
 const app = express();
 
-var whitelist = [process.env.REACT_APP_FRONTEND_URL, 'http://localhost:3000']
+// var whitelist = [process.env.REACT_APP_FRONTEND_URL, 'http://localhost:3000']
 
-app.use(function(req, res, next) {
-  const origin = req.header('Origin');
-  console.log("hey");
-  res.setHeader("Access-Control-Allow-Origin", origin!);
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+// app.use(function(req, res, next) {
+//   const origin = req.header('Origin');
+//   console.log("hey");
+//   res.setHeader("Access-Control-Allow-Origin", origin!);
+//   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
   
-});
+// });
 
-var corsOptions = {
-  origin: true,
-  optionsSuccessStatus: 200,
-  methods: "GET,POST,OPTIONS,DELETE,PUT",
-  // allowedHeaders: ["Content-Type", "Authorization"]
-}
+// var corsOptions = {
+//   origin: true,
+//   optionsSuccessStatus: 200,
+//   methods: "GET,POST,OPTIONS,DELETE,PUT",
+//   // allowedHeaders: ["Content-Type", "Authorization"]
+// }
 
-app.use(cors(corsOptions));
-app.options("*", (req, res) => {
-  const origin = req.header('Origin');
-  res.setHeader("Access-Control-Allow-Origin", origin!);
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.sendStatus(200);
-});
+// app.use(cors(corsOptions));
+// app.options("*", (req, res) => {
+//   const origin = req.header('Origin');
+//   res.setHeader("Access-Control-Allow-Origin", origin!);
+//   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.sendStatus(200);
+// });
 
 
 
@@ -51,14 +51,7 @@ app.use(express.json());
 // app.use(express.static(path.join(__dirname, '../app/public')));
 
 // Use routes
-app.use('/api/users', userUpdateRoutes);
-app.use('/api', searchRoutes);
-app.use('/api', showUserInfoRoutes);
-app.use('/api', makePostsRoutes);
-app.use('/api', makeCommentsRoutes);
-app.use('/api', updateRelationRoutes);
-app.use('/api', showRelationRoutes);
-
+// app
 
 const PORT = process.env.PORT || 3001;
 
